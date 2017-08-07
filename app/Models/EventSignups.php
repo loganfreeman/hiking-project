904726ee8221a0ff95cc8801179a24da1c9212bc
shanhong cheng <shanhong.cheng@utah.edu>
2017-08-07 13:32:18 -0600

    event signups

--- app/Models/EventSignups.php
+++ app/Models/EventSignups.php
@@ -1,10 +1,40 @@
 <?php
 
-namespace GrahamCampbell\BootstrapCMS;
+namespace GrahamCampbell\BootstrapCMS\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class EventSignups extends Model
+use GrahamCampbell\Credentials\Models\AbstractModel;
+
+class EventSignups extends AbstractModel
 {
-    //
+    protected $fillable = ['event_id', 'user_id'];
+
+    /**
+     * The table the comments are stored in.
+     *
+     * @var string
+     */
+    protected $table = 'event_signups';
+
+    /**
+     * The model name.
+     *
+     * @var string
+     */
+    public static $name = 'event_signup';
+
+    /**
+     * The columns to order by when displaying an index.
+     *
+     * @var string
+     */
+    public static $order = 'id';
+
+    /**
+     * The direction to order by when displaying an index.
+     *
+     * @var string
+     */
+    public static $sort = 'desc';
 }
